e4f0b29c3665234cae19a5e005828355c43ecca5
Aldeny Junior <aldenyjr.dev@gmail.com>
2023-09-18 16:50:01 -0300

    Avançando no curso

---
+++ src/edu/aldeny/primeirasemana/JavaTerminal.java
@@ -0,0 +1,7 @@
+package edu.aldeny.primeirasemana;
+
+public class JavaTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
